0fb60f08d3358f2be39d2dea21f5d5ef71a6b3b7
luctranquoc <luctranquoc@gmail.com>
2018-05-24 22:50:33 +0700

    Change-Id: I0000000000000000000000000000000000000000

--- src/pages/google_maps/google_maps.ts
+++ src/pages/google_maps/google_maps.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { GoogleMaps, GoogleMap, GoogleMapsEvent } from '@ionic-native/google-maps';
+import { NavController, AlertController } from 'ionic-angular';
+import { GoogleMaps, GoogleMap, GoogleMapsEvent, Marker, Geocoder, GeocoderResult } from '@ionic-native/google-maps';
 
 @Component({
   selector: 'page-google_maps',
@@ -8,9 +8,10 @@ import { GoogleMaps, GoogleMap, GoogleMapsEvent } from '@ionic-native/google-map
 })
 export class TestGoogleMapsPage {
 	map: GoogleMap;
+	search_address: string;
 
-	  constructor(public navCtrl: NavController) {
-		this.loadMap();
+	  constructor(public navCtrl: NavController, public alertCtrl: AlertController) {
+		this.loadMap();		
 	  }
 	
 	loadMap() {
@@ -18,4 +19,45 @@ export class TestGoogleMapsPage {
    		this.map = GoogleMaps.create('map_canvas');
 	}
 
+	showMarker(atitle: any, alat: any, along: any){
+		let marker: Marker = this.map.addMarkerSync({
+		  title: atitle,
+		  icon: 'blue',
+		  animation: 'DROP',
+		  position: {
+		    lat: alat,
+		    lng: along
+		  }
+		});		
+		marker.showInfoWindow();
+	}
+	
+	onButtonClick(event){	
+		// Address -> latitude,longitude
+		Geocoder.geocode({
+		  "address": this.search_address
+		})
+		.then((results: GeocoderResult[]) => {		
+			this.presentAlert(JSON.stringify(results[0].position));
+			let marker: Marker = this.map.addMarkerSync({
+			  title: JSON.stringify(results[0].position),
+			  icon: 'blue',
+			  animation: 'DROP',
+			  position: results[0].position
+			});		
+			marker.showInfoWindow();
+		});
+	}
+	
+	
+
+  //show alert
+	presentAlert(message : string) {
+	  let alert = this.alertCtrl.create({
+		title: 'Test alert',
+		subTitle: message,
+		buttons: ['OK']
+	  });
+	  alert.present();
+	}
 }
